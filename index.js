82542c82f72c1afb2246f3cafd3b4f0bfbb50b32
yodastea <78286019+yodastea@users.noreply.github.com>
2021-03-11 16:22:48 -0700

    fix1

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-function toggleSidebar(ref){
-    document.getElementById("sidebar").classList.toggle('active');
-  }
-  
+var menuToggle = document.querySelector("#menu-toggle");
+var activeElements = document.querySelectorAll(".active-element");
+var toggledMenu = menuToggle.addEventListener("click", function(){
+     // forEach is not supported in IE11
+  // activeElements.forEach(function(e){
+  //     e.classList.toggle("active");
+  // });
+     for(var activated = 0; activated < activeElements.length; activated++){
+          activeElements[activated].classList.toggle("active");
+     }
+})
