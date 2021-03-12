6f722fcf4da1e685a67967fc283e7ca26de3b558
yodastea <78286019+yodastea@users.noreply.github.com>
2021-03-11 19:52:52 -0700

    kek

--- index.js
+++ index.js
@@ -1,41 +1,6 @@
-var $xBar = $(".x-bar"),
-  $sideNav = $(".side-nav"),
-  $menuIcon = $(".menu-icon");
-$menuIcon.click(function() {
-  $sideNav.css({
-    left: 0
-  });
-  $menuIcon.fadeOut(5);
-  $xBar.css({
-    top: 30
-  });
-  if ($(window).width() < 767) {
-    $sideNav.css({
-      left: 0
-    });
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
   }
-  if ($(window).width() < 992) {
-    $sideNav.css({
-      width: 270
-    });
-  }
-});
-$xBar.click(function() {
-  $sideNav.css({
-    left: -307
-  });
-  $menuIcon.fadeIn(500);
-  $xBar.css({
-    top: -30
-  });
-  if ($(window).width() < 767) {
-    $sideNav.css({
-      left: -100 + "%"
-    });
-  }
-  if ($(window).width() < 991 && $(window).width() > 1199) {
-    $sideNav.css({
-      left: -270
-    });
-  }
-});
+);
+
